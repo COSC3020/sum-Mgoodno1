af12f32121f033dfd351d3a0315200fdc4334024
Mgoodno1 <90008902+Mgoodno1@users.noreply.github.com>
2024-10-28 23:04:48 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,15 @@
+const { array } = require("jsverify");
+
 function sum(a) {
     var sum = a[0];
-    for(var i = 0; i < a.length; i++) {
+    for(var i = 1; i < a.length; i++) {
         sum += a[i];
     }
+    if (a.length == 0) {
+        sum = 0;
+    }
+    console.log("Length",a.length)
+    console.log("Array:(",a)
+    console.log("Sum:(", sum)
     return sum;
 }
